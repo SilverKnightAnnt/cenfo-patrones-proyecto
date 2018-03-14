85c54d7f33cbfa40dd9a50c1606a619136db4180
Daniel Rodríguez <34356954+D-Roddie@users.noreply.github.com>
2018-03-14 13:50:09 -0600

    Creación de las clases: Raza, Enemigo y Mision.

--- DuelistSpirit/src/main/java/com/cenfotec/patrones/Raza.java
+++ DuelistSpirit/src/main/java/com/cenfotec/patrones/Raza.java
@@ -1,5 +1,54 @@
 package com.cenfotec.patrones;
 
 public class Raza {
+	private String nombre;
+	private int punteria;
+	private int vida_max;
+	private int inteligencia;
+	private int magia;
+
+	public Raza() {
+
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getPunteria() {
+		return punteria;
+	}
+
+	public void setPunteria(int punteria) {
+		this.punteria = punteria;
+	}
+
+	public int getVida_max() {
+		return vida_max;
+	}
+
+	public void setVida_max(int vida_max) {
+		this.vida_max = vida_max;
+	}
+
+	public int getInteligencia() {
+		return inteligencia;
+	}
+
+	public void setInteligencia(int inteligencia) {
+		this.inteligencia = inteligencia;
+	}
+
+	public int getMagia() {
+		return magia;
+	}
+
+	public void setMagia(int magia) {
+		this.magia = magia;
+	}
 
 }
