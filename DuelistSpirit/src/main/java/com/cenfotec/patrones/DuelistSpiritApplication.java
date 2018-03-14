4dce4db4fa94855dbb1361e9358d107d2000fcba
Ángel Marín <35677091+SilverKnightAnnt@users.noreply.github.com>
2018-03-14 17:27:00 -0600

    Clases Mapa, Mundo, Partida, Entidad y UI
    Esto fue hecho por José Andrés Salazar, pero por un problema técnico, no puedo subirlo desde su perfil.

---
+++ DuelistSpirit/src/main/java/com/cenfotec/patrones/DuelistSpiritApplication.java
@@ -0,0 +1,12 @@
+package com.cenfotec.patrones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuelistSpiritApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DuelistSpiritApplication.class, args);
+	}
+}
